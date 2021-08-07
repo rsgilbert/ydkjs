f244b7b23b4687cc358c2998eb51240166c1a796
rsgilbert <gssenyonjo@gmail.com>
2021-08-07 11:19:31 +0300

    explored typeof

---
+++ ch3-types/types/types.js
@@ -0,0 +1,32 @@
+log = console.log;
+
+/*
+log(typeof undefined)
+log(typeof true)
+log(typeof 42)
+log(typeof "Great")
+log(typeof { a: 2 })
+log(typeof Symbol())
+log(typeof null)
+log(typeof(1))
+
+let a = null;
+log(!a && typeof a === 'object')
+
+log(typeof function(){})
+
+function f1(a) {}
+function f2(a, b){}
+function f0() {}
+log(f1.length)
+log(f2.length)
+log(f0.length)
+
+log (typeof [])
+log(typeof [4, 2])
+
+log([2, 5, 3]["1"])
+
+log(typeof(kd))
+*/
+
